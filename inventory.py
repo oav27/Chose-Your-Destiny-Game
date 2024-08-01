8002ae9c009f0cbca54040ac697ef045f7f72a17
Olivia <160529667+oav27@users.noreply.github.com>
2024-07-31 18:45:45 -0600

    Add files via upload

---
+++ inventory.py
@@ -0,0 +1,18 @@
+#Backpack for the swamp
+s_backpack = ["taco", "sharp stick", "bug repellent"]
+
+#Will always tell the user what's in the backpack
+def swamp_backpack():
+  global s_backpack
+  print("In your backpack you have:")
+  print(s_backpack)
+
+#Backpack for the beach
+di_backpack = ["expired soda", "fishing rod", "sunscreen"]
+
+#Will always tell the user what's in the backpack
+def d_i_backpack():
+  global di_backpack
+  print("In your backpack you have:")
+  print(di_backpack)
+  
